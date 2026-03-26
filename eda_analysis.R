d34b38383053b3641a2769e2f84b0717ddf67c49
ying zhu <a1984460@adelaide.edu.au>
2026-03-26 20:56:52 +0800

    Fitting trend models to TFR using TSLM

--- eda_analysis.R
+++ eda_analysis.R
@@ -28,3 +28,11 @@ births<-birth_raw|>
 #Create a new tsibble for this data subset
 tfr_data <- births |> filter(DataSeries == "Total Fertility Rate (TFR)")
 tlb_data <- births |> filter(DataSeries == "Total Live-Births")
+
+## Fit three types of trend models to compare their performance
+tfr_fit <- tfr_data |>
+  model(
+    linear = TSLM(value ~ trend()),
+    quad   = TSLM(value ~ trend() + I(trend()^2)),
+    cubic  = TSLM(value ~ trend() + I(trend()^2) + I(trend()^3))
+  )
